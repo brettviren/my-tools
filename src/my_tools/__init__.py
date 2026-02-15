bf76cf7b03fd1f3922baee149409c143b4c49296
Brett Viren <brett.viren@gmail.com>
2026-02-15 12:50:56 -0500

    Add simple cli entry to quell uv tool install complaints

--- src/my_tools/__init__.py
+++ src/my_tools/__init__.py
@@ -1,2 +1,10 @@
 def hello() -> str:
     return "Hello from my-tools!"
+def main():
+    print("""
+    The my-tools umbrella.
+
+    $ uv tool install git+https://github.com/brettviren/my-tools
+
+    $ uv tool upgrade my-tools
+    """
